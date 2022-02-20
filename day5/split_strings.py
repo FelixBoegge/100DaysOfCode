c282cbd004ce6ce5bd57637bd5ae3cadb875aa66
FelixBoegge <72453751+FelixBoegge@users.noreply.github.com>
2022-02-20 21:23:59 +0000

    day5 start practice string methods

---
+++ day5/split_strings.py
@@ -0,0 +1,10 @@
+
+# training string
+s = 'The Tiger eats the Goat'
+
+print(s)
+print(s.split(' ')[1].split('g'))
+print(s.casefold())
+print(s.center(50, '-'))
+print(s.count('Goat', 0, 23))
+print(s.encode(encoding='ascii', errors='replace'))
