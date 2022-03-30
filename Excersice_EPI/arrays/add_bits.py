e613e4729fccb1be3be96c5e10501f11c2c432ab
FelixBoegge <72453751+FelixBoegge@users.noreply.github.com>
2022-03-30 15:36:51 +0000

    study Elements of Programming Invertviews in Python arrays

---
+++ Excersice_EPI/arrays/add_bits.py
@@ -0,0 +1,17 @@
+def add_bits(Bs, Bt):
+    length = max(len(Bs), len(Bt))
+    for i in reversed(range(length)):
+        Bs[i] += Bt[i]
+        if Bs[i] > 1:
+            Bs[i] -= 2
+            if i > 0:
+                Bs[i - 1] += 1
+            else:
+                Bs.insert(0, 1)
+    return Bs
+
+Bs = [0,1,1,1,1,1,0]
+Bt = [0,1,1,1,0,0,1]
+print(Bs)
+print(Bt)
+print(add_bits(Bs, Bt))
