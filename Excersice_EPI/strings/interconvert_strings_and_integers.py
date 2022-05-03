77a3670c8ae5beb6eded7d4eb646963378a08554
FelixBoegge <72453751+FelixBoegge@users.noreply.github.com>
2022-05-03 16:36:41 +0000

    EPI strings interconvert strings and integers - convert strings to ints and ints to strings without using python built-in fucitons str() and int()

---
+++ Excersice_EPI/strings/interconvert_strings_and_integers.py
@@ -0,0 +1,28 @@
+import functools
+import string
+
+
+def int_to_string(x: int) -> str:
+    is_negative = False
+    if x < 0:
+        x, is_negative = -x, True
+
+    s = []
+    while True:
+        s.append(chr(ord('0') + x % 10))
+        x //= 10
+        if x == 0:
+            break
+
+    return ('-' if is_negative else '') + ''.join(reversed(s))
+
+def string_to_int(s: str) -> int:
+    return (-1 if s[0] == '-' else 1) * functools.reduce(lambda running_sum, c: running_sum * 10 + string.digits.index(c), s[s[0] in '-+':], 0)
+
+x = 123
+y = -234
+s = '+45'
+
+print(int_to_string(x))
+print(int_to_string(y))
+print(string_to_int(s))
