218d8214d86969a494bc786af6fdc2bbba903ba2
FelixBoegge <72453751+FelixBoegge@users.noreply.github.com>
2022-05-17 16:35:02 +0000

    EPI strings check palindromicity added a more pythonic approach

--- Excersice_EPI/strings/palindromicity.py
+++ Excersice_EPI/strings/palindromicity.py
@@ -11,7 +11,21 @@ def is_palindrome(s: str) -> bool:
         j -= 1
     return True
 
+# pythonic approach
+def is_pal(s: str) -> bool:
+    return all(
+        a == b
+        for a, b in zip(map(str.lower, filter(str.isalnum, s)),
+                        map(str.lower, filter(str.isalnum, reversed(s))))
+    )
+
 a = "Ray a Ray"
 b = "A man, a plan, a canal, Panama!"
 c = "Able was I, ere I saw Elba"
+print(is_palindrome(a))
+print(is_palindrome(b))
 print(is_palindrome(c))
+print(is_pal(a))
+print(is_pal(b))
+print(is_pal(c))
+
