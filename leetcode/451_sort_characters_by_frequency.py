8a062facdb6bff0dd826c3222fa65c6a5a204aa6
FelixBoegge <72453751+FelixBoegge@users.noreply.github.com>
2022-05-21 14:17:11 +0000

    leetcode roman to integer sort characters by frequency

---
+++ leetcode/451_sort_characters_by_frequency.py
@@ -0,0 +1,16 @@
+def frequencySort(s: str) -> str:
+    count = {}
+    freq = []
+    out = ""
+    for c in s:
+        if c in count.keys():
+            count[c] += 1
+        else:
+            count[c] = 1
+    freq = dict(sorted(count.items(), key=lambda item: item[1], reverse=True))
+    for key, value in freq.items():
+        out += value * key
+    return out
+
+s = 'treett'
+print(frequencySort(s))
