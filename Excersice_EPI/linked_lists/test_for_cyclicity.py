8dfd37334b70373f901b4d183ad1691ee6ff26fc
FelixBoegge <72453751+FelixBoegge@users.noreply.github.com>
2022-05-23 16:44:34 +0000

    EPI linked lists test for cyclicity variant - without calculating the len of the cycle

--- Excersice_EPI/linked_lists/test_for_cyclicity.py
+++ Excersice_EPI/linked_lists/test_for_cyclicity.py
@@ -39,3 +39,16 @@ def has_cylce(head):
 
 
 print(f"cycle start: {has_cylce(M.head)[0]}", f"cycle length: {has_cylce(M.head)[1]}", sep="\n")
+
+def has_cycle2(head):
+    slow = fast = head
+    while fast and fast.next and fast.next.next:
+        slow, fast = slow.next, fast.next.next
+        if slow is fast:
+            slow = head
+            while slow is not fast:
+                slow, fast = slow.next, fast.next
+            return slow.data
+    return None
+
+print(has_cycle2(M.head))
