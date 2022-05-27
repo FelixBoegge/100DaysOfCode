01b98974fc4855bafe3b77b6cde56062b24f4cc0
FelixBoegge <72453751+FelixBoegge@users.noreply.github.com>
2022-05-27 14:30:21 +0000

    EPI stacks and queues validade parenthesis

---
+++ Excersice_EPI/stacks_queues/valid_parenthesis.py
@@ -0,0 +1,17 @@
+from stack import Stack
+
+
+def valid_parenthesis(par):
+    s = Stack()
+    pairs = {'(': ')', '[':']', '{': '}'}
+    for p in par:
+        if p in pairs:
+            s.push(p)
+        elif s.empty() or pairs[s.pop()] != p:
+                return False
+    return not s.empty()
+
+a = '()[]{}'
+b = '[{)){[}'
+
+print(valid_parenthesis(a))
