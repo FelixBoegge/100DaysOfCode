da4edf5fbc838f43ad6b47d7cd9c2bbad70c01af
FelixBoegge <72453751+FelixBoegge@users.noreply.github.com>
2022-05-27 13:08:06 +0000

    EPI stacks and queues implement a stack class with max function

---
+++ Excersice_EPI/stacks_queues/stack.py
@@ -0,0 +1,34 @@
+import collections
+
+
+class Stack:
+    StackWithCacheMax = collections.namedtuple('StackWithCacheMax', ('value', 'max'))
+
+    def __init__(self):
+        self.stack : [Stack.StackWithCacheMax] = []
+
+    def push(self, val):
+        self.stack.append(self.StackWithCacheMax(val, val if self.empty() else max(val, self.max())))
+
+    def pop(self):
+        return self.stack.pop().value
+
+    def peak(self):
+        return self.stack[-1]
+
+    def empty(self):
+        return len(self.stack) == 0
+
+    def max(self):
+        return self.stack[-1].max
+
+
+s = Stack()
+s.push(1)
+s.push(2)
+s.push(3)
+s.push(4)
+s.push(2)
+s.pop()
+
+print(s.max())
