724528a051cc1ee7ee0359be51927ef77d8ac906
FelixBoegge <72453751+FelixBoegge@users.noreply.github.com>
2022-05-27 13:31:47 +0000

    EPI stacks and queues evaluate RPN expressions with a stack

---
+++ Excersice_EPI/stacks_queues/evaluate_RPN_expressions.py
@@ -0,0 +1,21 @@
+from stack import Stack
+
+
+def evaluate(exp):
+    inter_res = Stack()
+    delimiter = ','
+    operations = {'+': lambda y,x : x+y,
+                  '-': lambda y,x : x-y,
+                  'x': lambda y,x : x*y,
+                  '/': lambda y,x : x//y}
+
+    for element in exp.split(delimiter):
+        if element in operations:
+            inter_res.push(operations[element](inter_res.pop(), inter_res.pop()))
+        else:
+            inter_res.push(int(element))
+
+    return inter_res.pop()
+
+exp = '3,4,+,3,x,1,+'
+print(evaluate(exp))
