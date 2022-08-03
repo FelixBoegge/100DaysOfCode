a58a3b6ad70a3ec5b2d77cb503082cd11b2089a9
FelixBoegge <72453751+FelixBoegge@users.noreply.github.com>
2022-08-03 12:09:38 +0200

    EPI binary search find missing IP address

---
+++ Excersice_EPI/binary_search/find_missing_IP_address.py
@@ -0,0 +1,23 @@
+import itertools
+
+
+def find_missing_element(stream: [int]) -> int:
+    num_bucket = 1 << 16
+    counter = [0] * num_bucket
+    stream, stream_copy = itertools.tee(stream)
+    for x in stream:
+        upper_part_x = x >> 16
+        counter[upper_part_x] += 1
+
+    bucket_capacity = 1 << 16
+    candidate_bucket = next(i for i,c in enumerate(counter) if c < bucket_capacity)
+    candidates = [0] * bucket_capacity
+    for x in stream_copy:
+        upper_part_x = x >> 16
+        if candidate_bucket == upper_part_x:
+            lower_part_x = ((1 << 16) - 1) & x
+            candidates[lower_part_x] = 1
+
+    for i, v in enumerate(candidates):
+        if v == 0:
+            return (candidate_bucket << 16) | i
