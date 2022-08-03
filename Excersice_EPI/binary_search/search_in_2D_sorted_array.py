a532986c430e34e5960ee4b8b9d63f4bda1a2f8d
FelixBoegge <72453751+FelixBoegge@users.noreply.github.com>
2022-08-03 07:39:35 +0200

    EPI binary search search in a 2D sorted array

---
+++ Excersice_EPI/binary_search/search_in_2D_sorted_array.py
@@ -0,0 +1,20 @@
+def matrix_search(A: [[int]], x: int) -> bool:
+    row, col = 0, len(A[0]) - 1
+    while row < len(A) and col >= 0:
+        if A[row][col] == x:
+            return True
+        elif A[row][col] < x:
+            row += 1
+        else:
+            col -= 1
+    return False
+
+
+A = [[-1, 2, 4, 4, 6],
+     [1, 5, 5, 9, 21],
+     [3, 6, 6, 9, 22],
+     [3, 6, 8, 10, 24],
+     [6, 8, 9, 12, 25],
+     [8, 10, 12, 13, 40]]
+
+print(matrix_search(A, x=7))
