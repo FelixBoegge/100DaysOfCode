291b5e225e95a3ef07d2705af2e17bba62bee23d
FelixBoegge <72453751+FelixBoegge@users.noreply.github.com>
2022-08-10 17:52:01 +0200

    EPI hash tables find the nearest repeated entries in an array

--- Excersice_EPI/hash tables/nearest_repetition_of_entry.py
+++ Excersice_EPI/hash tables/nearest_repetition_of_entry.py
@@ -6,4 +6,7 @@ def find_nearest_repetition(paragraph: [str]) -> int:
             latest_equal_word = word_to_latest_index[word]
             nearest_repeated_distance = min(nearest_repeated_distance, i - latest_equal_word)
         word_to_latest_index[word] = i
-    return typing.cast(int, nearest_repeated_distance) if nearest_repeated_distance != float('inf') else -1
+    return nearest_repeated_distance if nearest_repeated_distance != float('inf') else -1
+
+paragraph = ['All', 'work', 'and', 'no', 'play', 'makes', 'for', 'no', 'work', 'no', 'fun', 'and', 'no', 'result']
+print(find_nearest_repetition(paragraph))
