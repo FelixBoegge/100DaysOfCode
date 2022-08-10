f79e1fc9809f3ae58a83ad75b7f83f22ec2062ed
FelixBoegge <72453751+FelixBoegge@users.noreply.github.com>
2022-08-10 17:14:00 +0200

    EPI hash tables lca optimization with a hash table

--- Excersice_EPI/hash tables/LCA.py
+++ Excersice_EPI/hash tables/LCA.py
@@ -1,1 +1,19 @@
+from binary_tree_node import BinaryTreeNode
+
 def lca(node0: BinaryTreeNode, node1: BinaryTreeNode) -> BinaryTreeNode:
+    iter0, iter1 = node0, node1
+    nodes_on_path_to_root = set()
+    while iter0 or iter1:
+        if iter0:
+            if iter0 in nodes_on_path_to_root:
+                return iter0
+            nodes_on_path_to_root.add(iter0)
+            iter0 = iter0.parent
+        if iter1:
+            if iter1 in nodes_on_path_to_root:
+                return iter1
+            nodes_on_path_to_root.add(iter1)
+            iter1 = iter1.parent
+    raise ValueError('node0 and node1 are not in the same tree')
+
+
